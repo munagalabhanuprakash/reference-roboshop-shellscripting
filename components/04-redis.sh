c1e524a0d21c90785b39e3351a63b9b61be1f8ef
username <bhanuprakashmunagala@gmail.com>
2022-05-06 18:48:31 +0530

    added redis to makefile

--- components/04-redis.sh
+++ components/04-redis.sh
@@ -12,8 +12,8 @@ yum install redis-6.2.7 -y &>>${LOG_FILE}
 CheckStatus $?
 
 ECHO "Updating Bind ip in /etc/redis.conf and /etc/redis/redis.conf"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
-
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf &>>${LOG_FILE}
+CheckStatus $?
 # systemctl enable redis
 # systemctl start redis
 
