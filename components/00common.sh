978015418fe98eef42172140dae43a7b7df1cd4d
username <bhanuprakashmunagala@gmail.com>
2022-05-06 12:15:17 +0530

    Removed Old files and added new

--- components/00common.sh
+++ components/00common.sh
@@ -14,4 +14,27 @@ fi
 }
 #-----------------------------------------------------------------------------------------------------------------------
 
-CheckRootUser
+#This function is to check that the command has been successfully executed or not---------------------------------------
+CheckStatus()
+{
+  if [ "$?" -eq  "0" ]; then
+     echo -e "----SUCCESS----\n"
+  else
+    echo -e "----FAILURE----\n"
+    echo Check Error Log for more info
+ fi
+}
+#-----------------------------------------------------------------------------------------------------------------------
+
+# This global variable is to creat and reuse the log file acoos the project
+LOG_FILE=/tmp/roboshop.log
+rm -f $LOG_FILE
+#-----------------------------------------------------------------------------------------------------------------------
+
+#This function redirects the output to the log file aswell to check status of each and every step-----------------------
+ECHO()
+{
+  echo -e "=========================== $1 ===========================\n" >>${LOG_FILE}
+  echo "$1"
+}
+#-----------------------------------------------------------------------------------------------------------------------
