fe4d25d3f83340829456fd4b4cc95e33d37c8e17
username <bhanuprakashmunagala@gmail.com>
2022-05-06 17:05:32 +0530

    Removed Old files and added new

--- components/03-catalogue.sh
+++ components/03-catalogue.sh
@@ -11,20 +11,13 @@ ECHO "Installing NodeJS and Compiler"
 yum install nodejs gcc-c++ -y &>>${LOG_FILE}
 CheckStatus $?
 
-ECHO "Removing any existing roboshop user and Adding roboshop User"
-# userdel roboshop &>>${LOG_FILE}
-useradd roboshop &>>${LOG_FILE}
-CheckStatus $?
-
-ECHO "Add Application User"
-useradd roboshop &>>${LOG_FILE}
-CheckStatus $?
-
 id roboshop &>>${LOG_FILE}
 if [ $? -ne 0 ]; then
     echo "Adding Application(roboshop) user"
     useradd roboshop &>>${LOG_FILE}
 fi
+
+ls -l
 # Note: Here We are checking whether the roboshop user already exists or not by checking the status code (0 if Exists and Non zero if user exists)and if not we are creating one
 
 
