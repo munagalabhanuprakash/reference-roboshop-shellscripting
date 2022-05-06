8d841368bf331a2b8805a09728b4fb8b0b7faa91
username <bhanuprakashmunagala@gmail.com>
2022-05-06 18:43:23 +0530

    added redis to makefile

--- components/04-redis.sh
+++ components/04-redis.sh
@@ -4,11 +4,11 @@ source /home/centos/roboshop/components/00-common.sh
 CheckRootUser
 
 ECHO "Configuring redis repos"
-curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG_FILE}
 CheckStatus $?
 
 ECHO "Installing redis-6.2.7"
-yum install redis-6.2.7 -y
+yum install redis-6.2.7 -y &>>${LOG_FILE}
 CheckStatus $?
 
 #ECHO "Updating Bind ip in /etc/redis.conf and /etc/redis/redis.conf"
