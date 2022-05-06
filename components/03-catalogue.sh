6a0c8ace92cc22ecdd268f82879993c46179e689
username <bhanuprakashmunagala@gmail.com>
2022-05-06 17:17:13 +0530

    Removed Old files and added new

--- components/03-catalogue.sh
+++ components/03-catalogue.sh
@@ -16,10 +16,28 @@ if [ $? -ne 0 ]; then
     echo "Adding Application(roboshop) user"
     useradd roboshop &>>${LOG_FILE}
 fi
-
-ls -l
 # Note: Here We are checking whether the roboshop user already exists or not by checking the status code (0 if Exists and Non zero if user exists)and if not we are creating one
+$ npm install
 
+ECHO "Downloading the content"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
+CheckStatus $?
+
+ECHO "Moving to roboshop folder and unzipping the file"
+cd /home/roboshop &>>${LOG_FILE} && unzip -o /tmp/catalogue.zip &>>${LOG_FILE}
+CheckStatus $?
+
+ECHO "Moving catalogue-main folder to catalogue"
+mv catalogue-main catalogue
+CheckStatus $?
+
+ECHO "change directory to catalogue directory"
+cd /home/roboshop/catalogue
+CheckStatus $?
+
+ECHO "Installing npm"
+npm install
+CheckStatus $?
 
 
 
