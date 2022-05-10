b11811d8848476804d9e25d48ad9f00557630bd4
username <bhanuprakashmunagala@gmail.com>
2022-05-10 19:39:46 +0530

    edited common file

--- create-ec2-instance/create-ec2-instance.sh
+++ create-ec2-instance/create-ec2-instance.sh
@@ -28,5 +28,5 @@ INSTANCE_ID=$(aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Va
 IPADDRESS=$(aws ec2 describe-instances  --instance-ids ${INSTANCE_ID}  --output table | grep PrivateIpAddress | head -n 1 | awk '{print $4}')
 
 sed -e "s/COMPONENT/${NAME}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id Z0616702OUXACTER2CD1 --change-batch file:////tmp/record.json &>/dev/null
+aws route53 change-resource-record-sets --hosted-zone-id Z0616702OUXACTER2CD1 --change-batch file:///tmp/record.json &>/dev/null
 echo DNS Record Created
